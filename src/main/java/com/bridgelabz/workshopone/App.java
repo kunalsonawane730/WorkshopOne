e400034596d58f0f191bd27837056f9d8faea008
Kunal <kunalsonawane730@gmail.com>
2022-11-19 08:58:48 +0530

    adding welcome message

---
+++ src/main/java/com/bridgelabz/workshopone/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.workshopone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome To Workshop" );
+    }
+}
